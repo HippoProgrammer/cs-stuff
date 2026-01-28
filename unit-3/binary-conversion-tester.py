64c03cfa569c89db21e1f9c825ee3559e8c03d78
HippoProgrammer <bcgcustomerservices@gmail.com>
2026-01-28 09:58:53 +0000

    QoL changes

--- unit-3/binary-conversion-tester.py
+++ unit-3/binary-conversion-tester.py
@@ -13,7 +13,7 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.'''
-import math, random
+import random, time
 print(''' binary-conversion-tester.py  Copyright (C) 2026  HippoProgrammer
     This program comes with ABSOLUTELY NO WARRANTY.
     This is free software, and you are welcome to redistribute it
@@ -22,55 +22,89 @@ print(''' binary-conversion-tester.py  Copyright (C) 2026  HippoProgrammer
 min_val = 1
 max_val = 256
 
-print('Binary Conversion Tester\nOptions:\n1.Denary -> Binary\n2.Binary -> Denary\n3.Denary -> Hex\n4.Hex -> Denary\n5.Binary -> Hex\n6.Hex -> Binary')
-option = int(input('Select an option (1/2/3/4/5/6): '))
+print('Binary Conversion Tester')
+print('Press CTRL-C to exit program.')
 while True:
-    int_value = random.randint(min_val,max_val)
-    if option == 1:
-        value = int_value
-        print(f'Convert {value}.')
-        answer = '{:b}'.format(int_value)
-        if input('Answer: ') == answer:
-            print(f'Correct! It is {answer}!')
-        else:
-            print(f'Incorrect. Correct answer: {answer}')
-    elif option == 2:
-        value = '{:b}'.format(int_value)
-        print(f'Convert {value}.')
-        answer = str(int_value)
-        if input('Answer: ') == answer:
-            print(f'Correct! It is {answer}!')
-        else:
-            print(f'Incorrect. Correct answer: {answer}')
-    elif option == 3:
-        value = int_value
-        print(f'Convert {value}.')
-        answer = '{:x}'.format(int_value)
-        if input('Answer: ') == answer:
-            print(f'Correct! It is {answer}!')
-        else:
-            print(f'Incorrect. Correct answer: {answer}')
-    elif option == 4:
-        value = '{:x}'.format(int_value)
-        print(f'Convert {value}.')
-        answer = str(int_value)
-        if input('Answer: ') == answer:
-            print(f'Correct! It is {answer}!')
-        else:
-            print(f'Incorrect. Correct answer: {answer}')
-    elif option == 5:
-        value = '{:b}'.format(int_value)
-        print(f'Convert {value}.')
-        answer = '{:x}'.format(int_value)
-        if input('Answer: ') == answer:
-            print(f'Correct! It is {answer}!')
-        else:
-            print(f'Incorrect. Correct answer: {answer}')
-    elif option == 6:
-        value = '{:x}'.format(int_value)
-        print(f'Convert {value}.')
-        answer = '{:b}'.format(int_value)
-        if input('Answer: ') == answer:
-            print(f'Correct! It is {answer}!')
-        else:
-            print(f'Incorrect. Correct answer: {answer}')
+    try:
+        print('Options:\n1.Denary -> Binary\n2.Binary -> Denary\n3.Denary -> Hex\n4.Hex -> Denary\n5.Binary -> Hex\n6.Hex -> Binary')
+        option = int(input('Select an option (1/2/3/4/5/6): '))
+        correct = 0
+        incorrect = 0
+        start_time = 0
+        questions = 0
+    except KeyboardInterrupt:
+        print('Exiting. Thanks for playing!')
+        break
+    print('Session started. Press CTRL-C to stop session.')
+    while True:
+        try:
+            start_time = time.time()
+            int_value = random.randint(min_val,max_val)
+            questions += 1
+            if option == 1:
+                value = int_value
+                print(f'Convert {value}.')
+                answer = '{:b}'.format(int_value)
+                if input('Answer: ') == answer:
+                    print(f'Correct! It is {answer}!')
+                    correct += 1
+                else:
+                    print(f'Incorrect. Correct answer: {answer}')
+                    incorrect += 1
+            elif option == 2:
+                value = '{:b}'.format(int_value)
+                print(f'Convert {value}.')
+                answer = str(int_value)
+                if input('Answer: ') == answer:
+                    print(f'Correct! It is {answer}!')
+                    correct += 1
+                else:
+                    print(f'Incorrect. Correct answer: {answer}')
+                    incorrect += 1
+            elif option == 3:
+                value = int_value
+                print(f'Convert {value}.')
+                answer = '{:x}'.format(int_value)
+                if input('Answer: ') == answer:
+                    print(f'Correct! It is {answer}!')
+                    correct += 1
+                else:
+                    print(f'Incorrect. Correct answer: {answer}')
+                    incorrect += 1
+            elif option == 4:
+                value = '{:x}'.format(int_value)
+                print(f'Convert {value}.')
+                answer = str(int_value)
+                if input('Answer: ') == answer:
+                    print(f'Correct! It is {answer}!')
+                    correct += 1
+                else:
+                    print(f'Incorrect. Correct answer: {answer}')
+                    incorrect += 1
+            elif option == 5:
+                value = '{:b}'.format(int_value)
+                print(f'Convert {value}.')
+                answer = '{:x}'.format(int_value)
+                if input('Answer: ') == answer:
+                    print(f'Correct! It is {answer}!')
+                    correct += 1
+                else:
+                    print(f'Incorrect. Correct answer: {answer}')
+                    incorrect += 1
+            elif option == 6:
+                value = '{:x}'.format(int_value)
+                print(f'Convert {value}.')
+                answer = '{:b}'.format(int_value)
+                if input('Answer: ') == answer:
+                    print(f'Correct! It is {answer}!')
+                    correct += 1
+                else:
+                    print(f'Incorrect. Correct answer: {answer}')
+                    incorrect += 1
+        except KeyboardInterrupt:
+            end_time = time.time()
+            total_time = end_time - start_time
+            avg_time = total_time / questions
+            percentage_correct = correct * 100 / questions
+            print(f'Session completed. {percentage_correct}% correct. {avg_time}s per question on average.')
+            break
